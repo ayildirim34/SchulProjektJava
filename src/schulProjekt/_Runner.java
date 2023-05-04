3581002cd87885a6b020e16aa9167b6b14fbadba
ayildirim34 <ayildirim3457@protonmail.com>
2023-05-04 13:34:55 +0200

    letzte

--- src/schulProjekt/_Runner.java
+++ src/schulProjekt/_Runner.java
@@ -2,8 +2,11 @@ package schulProjekt;
 
 public class _Runner {
     public static void main(String[] args) throws InterruptedException {
+
         Lehrer.fackeLehrerHinzufügen();
+
         Schüler.fackeSchülerHinzufügen();
+
         Lage.mainMenu();
     }
 }
